aab04d5b89224af85ea8aa5021efbfd457aad30f
Auri Gabriel <ririmello.a@gmail.com>
2025-04-20 15:51:41 -0300

    Pass selected answers to ResultsScreen and implement summary data generation

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -43,7 +43,7 @@ class _QuizState extends State<Quiz> {
     }
 
     if (activeScreen == 'results-screen') {
-      screenWidget = ResultsScreen();
+      screenWidget = ResultsScreen(chosenAnswers: selectedAnswers);
     }
 
     return MaterialApp(
